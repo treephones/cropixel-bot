e2f94891b8b857e6d8f20c4d08bd8d98f0ae998d
Moez B <moezbajwa@hotmail.com>
2020-11-21 17:40:46 -0500

    image editor working

--- src/main/java/com/treephones/cropixelbot/events/text/ImageCommands.java
+++ src/main/java/com/treephones/cropixelbot/events/text/ImageCommands.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -13,17 +14,18 @@ import com.treephones.cropixelbot.utils.Utils;
 
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.utils.AttachmentOption;
 
 public class ImageCommands extends ListenerAdapter {
-	public HashMap<String, BufferedImage> images = images();
+	public HashMap<String, BufferedImage> images = new HashMap<String, BufferedImage>();
 	
-	HashMap<String, BufferedImage> images() {
+	void imageSet() {
 		try {
-			this.images.put("frick", ImageIO.read(new File("resources/images/lex.jpg")));
+			this.images.put("frick", ImageIO.read(new File("src/main/resources/images/lex.jpg")));
+			this.images.put("brown", ImageIO.read(new File("src/main/resources/images/haddis.jpg")));
+			this.images.put("greek", ImageIO.read(new File("src/main/resources/images/metre.jpg")));
+			this.images.put("flip", ImageIO.read(new File("src/main/resources/images/flip.jpg")));
 		}
-		catch(IOException e) {}
-		return this.images;
+		catch(IOException e) {e.printStackTrace();}
 	}
 	
 	@Override
@@ -31,16 +33,18 @@ public class ImageCommands extends ListenerAdapter {
 		String[] cmd = Utils.getArgs(e);
 		
 		if(cmd[0].equalsIgnoreCase(Constants.prompt + "meme")) {
+			this.imageSet();
 			Graphics g = this.images.get(cmd[1]).getGraphics();
 			StringBuffer str = new StringBuffer();
 			for(int i = 2; i < cmd.length; ++i) {
 				str.append(cmd[i] + " ");
 			}
-			g.drawString(str.toString(), 20, 20);
+			g.setFont(g.getFont().deriveFont(40f));
+			g.drawString(str.toString(), 20, 100);
 			g.dispose();
 			try {
-				ImageIO.write(this.images.get(cmd[1]), "jpg", new File("resources/images/lex.jpg"));
-				e.getChannel().sendFile(new File("resources/images/lex.jpg")).queue();
+				ImageIO.write(this.images.get(cmd[1]), "jpg", new File("src/main/resources/editedcache/cache.jpg"));
+				e.getChannel().sendFile(new File("src/main/resources/editedcache/cache.jpg")).queue();
 			}
 			catch(IOException f) {}
 		}
