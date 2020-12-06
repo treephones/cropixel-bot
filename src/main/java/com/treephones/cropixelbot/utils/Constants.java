701deff16ba0b5293956cccfeeaa081edb393f5c
Moez <35158270+treephones@users.noreply.github.com>
2020-12-06 02:56:06 -0500

    Update Constants.java
    deploy prep

--- src/main/java/com/treephones/cropixelbot/utils/Constants.java
+++ src/main/java/com/treephones/cropixelbot/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final String prompt = "/";
 	
-	public static final String key_discord = "token"; 
+	public static final String key_discord = System.getenv("TOKEN"); 
 	public static final String key_hypixel = "326531eb-bc22-4a1a-8e1e-9c3565b6e8f0";
 	
 	public static final String task_channel = "hypixel-updates";
